8ee1f832b00bd9f4282738b32ceff144bb92ebab
Misuke2469 <crazytoto795@gmail.com>
2019-05-10 11:59:14 +0900

    Application Syokuma_map model ADD 2

--- Ssyokuma_map/models.py
+++ Ssyokuma_map/models.py
@@ -4,11 +4,28 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField('名前', max_length=20)
     address = models.CharField('住所', max_length=50)
-    lat = models.DecimalField('緯度', max_digits=8, decimal_places=6)
-    lng = models.DecimalField('経度', max_digits=9, decimal_places=6)
 
     def __str__(self):
         return str(self.name)
+
     class Meta:
         verbose_name = '顧客'
         verbose_name_plural = '顧客'
+
+
+class Shop(models.Model):
+    shop_id = models.CharField('店id', max_length=255, primary_key=True)
+    shop_name = models.CharField('店舗名', max_length=255)
+    shop_ruby = models.CharField('店舗名かな', max_length=255)
+    shop_address = models.CharField('店舗住所', max_length=255)
+    shop_lat = models.DecimalField('緯度', max_digits=8, decimal_places=6)
+    shop_lng = models.DecimalField('経度', max_digits=9, decimal_places=6)
+    shop_url = models.URLField('店舗URL', max_length=1000)
+    shop_tel = models.DecimalField('店電話番号', max_length=12)
+    shop_imageurl = models.CharField('店舗画像URL', max_length=1000)
+
+
+class Review(models.Model):
+    F_Judge = models.DecimalField('判断', max_length=20)
+    review_Scorenum = models.FloatField('店舗スコア', max_length=10)
+    review_Commentbody = models.CharField('コメエント', max_length=255)
